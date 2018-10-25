de794c9be12df34a716f1349efc9e926ddf1e9d9
Guru Ketepalle <mrteja.sekhar@gmail.com>
2018-10-25 22:18:37 +0530

    first commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Guru Ketepalle on 25/10/18.
+//  Copyright © 2018 Guru Ketepalle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
